fdfe231c74323f8e34767b1556dfe6e410693ba2
Brendan Beltz <brendanwebdev@gmail.com>
2023-02-27 19:51:29 -0500

    Full parity

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,47 +1,22 @@
-import { useState, useEffect, useRef, BaseSyntheticEvent } from "react";
-import { getRandomLocation } from "../../util/location";
-import { getStaticUrl } from "../../util/mapbox";
+import { useState, useEffect, useRef } from "react";
 import type { ILocationGuess, INewGameData } from "../../types";
 import { useHover } from "usehooks-ts";
 import LeafletEmbed from "../LeafletEmbed";
 import Draggable from "react-draggable";
 import Carousel from "./Carousel";
+import { calculateDistance, calculateScore } from "../../util/challenge";
 import "./Game.css";
 
-// TODO TOMORROW: Change submit approach to basic button.
-// Calculate score, update Previous guesses.
 // Probably will need to abstract further to make a subset component that reloads based on previousGuesses.
+// Code challenge by id. Persist to local storage.
 
-export default function Game() {
-  const [gameData, setGameData] = useState<INewGameData | null>(null);
-  useEffect(() => {
-    const getGameData = async () => {
-      const location = await getRandomLocation(10);
-      console.log(location);
-      const ZOOM_LEVELS = [15.5, 14, 12, 10, 8, 6.5, 3];
-      const { lat, lng } = location;
-      const images = ZOOM_LEVELS.map((zoom) =>
-        getStaticUrl({ lat, lng, zoom })
-      );
-      const gameData: INewGameData = { location, images };
-      setGameData(gameData);
-    };
-    getGameData();
-  }, []);
-  if (!gameData) {
-    return <p>Loading</p>;
-  }
-  return <GameScreen gameData={gameData} />;
-}
-
-export function GameScreen(props: { gameData: INewGameData }) {
-  const { gameData } = props;
+export default function Game(props: { challenge: INewGameData }) {
+  const { challenge } = props;
   const mapContainerRef = useRef(null);
   const isHover = useHover(mapContainerRef);
   // const guess = useFetcher();
   // const newGameData: INewGameData = loader();
-  const { location, images } = gameData;
-  const { lat, lng, id: locationId } = location;
+  const { location, images } = challenge;
   const [position, setPosition] = useState<null | { lat: number; lng: number }>(
     null
   );
@@ -49,9 +24,18 @@ export function GameScreen(props: { gameData: INewGameData }) {
   const [previousGuesses, setPreviousGuesses] = useState(
     [] as ILocationGuess[]
   );
-  const onGuessSubmit = (e: BaseSyntheticEvent) => {
-    e.preventDefault();
-    console.log(e);
+  const onGuessSubmit = () => {
+    const distance = calculateDistance(location, position!);
+    const score = calculateScore(100, distance);
+    if (distance >= 1000) {
+      console.debug(
+        Math.round((distance / 1000 + Number.EPSILON) * 10) / 10 + "km"
+      );
+    } else {
+      console.debug(Math.round(distance) + "m");
+    }
+    // https://overreacted.io/a-complete-guide-to-useeffect/#making-effects-self-sufficient
+    setPreviousGuesses((p) => [...p, { ...position!, score, distance }]);
   };
   useEffect(() => {
     document.addEventListener("click", (e) => {
@@ -69,22 +53,6 @@ export function GameScreen(props: { gameData: INewGameData }) {
   useEffect(() => {
     setMapActive(isHover);
   }, [isHover]);
-  // TODO: Replace with call to game.guess
-  // useEffect(() => {
-  //   if (guess.type === "done" && guess.data.distance) {
-  //     const distance: number = guess.data.distance;
-  //     console.log(guess);
-  //     if (distance >= 1000) {
-  //       console.log(
-  //         Math.round((distance / 1000 + Number.EPSILON) * 10) / 10 + "km"
-  //       );
-  //     } else {
-  //       console.log(Math.round(distance) + "m");
-  //     }
-  //     // https://overreacted.io/a-complete-guide-to-useeffect/#making-effects-self-sufficient
-  //     setPreviousGuesses((p) => [...p, { ...guess.data }]);
-  //   }
-  // }, [guess]);
 
   return (
     <>
@@ -116,19 +84,14 @@ export function GameScreen(props: { gameData: INewGameData }) {
             position={position}
             setPosition={setPosition}
           />
-          <form onSubmit={onGuessSubmit}>
-            <input type="hidden" name="locationId" value={locationId} />
-            {/* https://thewebdev.info/2021/09/18/how-to-fix-the-a-component-is-changing-an-uncontrolled-input-of-type-text-to-be-controlled-error-in-react/ */}
-            <input type="hidden" name="lat" value={position?.lat || 0} />
-            <input type="hidden" name="lng" value={position?.lng || 0} />
-            <button
-              type="submit"
-              disabled={position === null}
-              className="btn--submit"
-            >
-              Submit
-            </button>
-          </form>
+          <button
+            disabled={position === null}
+            className="btn--submit"
+            onClick={onGuessSubmit}
+          >
+            Submit
+          </button>
+
           <div id="map_drag_handle" className="drag-handle">
             ✥
           </div>
---
+++ src/util/challenge.ts
@@ -0,0 +1,64 @@
+import type { INewGameData } from "../types";
+import { getRandomLocation } from "./location";
+import { getStaticUrl } from "./mapbox";
+
+// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+
+export function haversineDistance(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) {
+  const p = 0.017453292519943295; // Math.PI / 180
+  const c = Math.cos;
+  const a =
+    0.5 -
+    c((lat2 - lat1) * p) / 2 +
+    (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+  return 12742000 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+export const generate = async () => {
+  const location = await getRandomLocation(10);
+  const ZOOM_LEVELS = [15.5, 14, 12, 10, 8, 6.5, 3];
+  const { lat, lng } = location;
+  const images = ZOOM_LEVELS.map((zoom) => getStaticUrl({ lat, lng, zoom }));
+  const gameData: INewGameData = { location, images };
+  return gameData;
+};
+
+export const calculateScore = (proportionBasis: number, distance: number) => {
+  if (distance <= proportionBasis) {
+    // Right location.
+    return 5;
+  } else if (distance <= proportionBasis * 20) {
+    // Right neighborhood.
+    return 4;
+  } else if (distance <= proportionBasis * 500) {
+    // Right metropolitan area.
+    return 3;
+  } else if (distance <= proportionBasis * 2000) {
+    // Rigth small country/province area.
+    return 2;
+  } else if (distance <= proportionBasis * 20000) {
+    // Right sub continent sized area.
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+export const calculateDistance = (
+  challengeCoords: { lat: number; lng: number },
+  guessCoords: { lat: number; lng: number }
+): number => {
+  const distance = haversineDistance(
+    challengeCoords.lat,
+    challengeCoords.lng,
+    guessCoords.lat,
+    guessCoords.lng
+  );
+  return distance;
+};
