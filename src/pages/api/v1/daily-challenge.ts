b642f0bba760896068e9c164ea472ac10f70bb58
Brendan Beltz <brendanwebdev@gmail.com>
2023-03-16 15:38:59 -0400

    Quick fix for missing top level await

--- src/pages/api/v1/daily-challenge.ts
+++ src/pages/api/v1/daily-challenge.ts
@@ -1,7 +1,9 @@
 import { generate } from "../../../util/challenge";
-const newChallenge = await generate();
 
 export async function get(): Promise<Response> {
+  // Cloudflare does not support top level await. This will now change the challenge every load instead of every build.
+  // TODO: Reference a daily challenge key somewhere else.
+  const newChallenge = await generate();
   // TODO: CORS
   return new Response(JSON.stringify(newChallenge), {
     status: 200,
